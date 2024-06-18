e59b66d5d4a1eaa7ea56bb777e1015fcfcdb3d22
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-18 13:13:10 +0000

    install react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
