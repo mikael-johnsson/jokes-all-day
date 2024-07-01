e33ce7d096310377b9c536eff528fcbb416d657e
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-07-01 14:17:36 +0000

    change Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Toast from 'react-bootstrap/Toast';
 import styles from '../styles/Toast.module.css'
 
-const ToastMessage = () => {
+const ToastMessage = (show) => {
   return (
-    <Toast className={styles.Toast} delay={3000} autohide>
+    <Toast className={styles.Toast} show={show} delay={3000} autohide>
         <Toast.Header closeButton={true}>
             <strong className="mr-auto">Data update</strong>
         </Toast.Header>
